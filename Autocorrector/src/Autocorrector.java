e74ebe3402ec2cd004e9eab2b473ed172fef8999
Logan Warner <loganswarnerprofessional@gmail.com>
2018-05-19 19:58:34 -0600

    Create input/output structure

--- Autocorrector/src/Autocorrector.java
+++ Autocorrector/src/Autocorrector.java
@@ -1,4 +1,12 @@
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
 
 /**
  * @author Logan Warner
@@ -7,12 +15,27 @@ public class Autocorrector {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         if(args.length < 1) {
             System.out.println("Usage: `java Autocorrector <inputfile>`");
             return;
         }
+        BufferedReader typos_in = new BufferedReader(new FileReader(new File(args[0])));
+        ArrayList<String> typo_words = new ArrayList<>();
+        
+        typos_in.lines().forEach((typo_word) -> {
+            typo_words.add(typo_word);
+        });
+        
+        ArrayList<String> corrected_words = new ArrayList<>();
+        
+        // TODO: populate array list with corrections
+        
+        corrected_words.forEach((corrected_word) -> {
+            System.out.println(corrected_word);
+        });
     }
     
 }
