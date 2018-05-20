a665bb95d362de5ef2fd87618dc3108ae6c00224
Logan Warner <loganswarnerprofessional@gmail.com>
2018-05-19 19:36:42 -0600

    Add main class

---
+++ Autocorrector/src/autocorrector/Autocorrector.java
@@ -0,0 +1,19 @@
+package autocorrector;
+
+/**
+ * @author Logan Warner
+ */
+public class Autocorrector {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if(args.length < 1) {
+            System.out.println("Usage: `java Autocorrector <inputfile>`");
+            return;
+        }
+        
+    }
+    
+}
