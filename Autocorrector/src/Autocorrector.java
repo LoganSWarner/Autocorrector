3e454bfeff7c9fde456baf8fd3ff4a9c3ac7bcc7
Logan Warner <loganswarnerprofessional@gmail.com>
2018-05-19 19:41:37 -0600

    Remove redundant directory layer

---
+++ Autocorrector/src/Autocorrector.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * @author Logan Warner
+ */
+public class Autocorrector {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if(args.length < 1) {
+            System.out.println("Usage: `java Autocorrector <inputfile>`");
+            return;
+        }
+    }
+    
+}
