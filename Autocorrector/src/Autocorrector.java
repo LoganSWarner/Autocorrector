d350fc945b4b8e0062271518b8a2d5139a072070
Logan Warner <loganswarnerprofessional@gmail.com>
2018-05-22 03:31:58 -0600

    Implement closest-match detection with cutoff of 2 D-L distance

--- Autocorrector/src/Autocorrector.java
+++ Autocorrector/src/Autocorrector.java
@@ -23,7 +23,7 @@ public class Autocorrector {
         }
         BufferedReader typos_in = new BufferedReader(new FileReader(new File(args[0])));
         ArrayList<String> typo_words = new ArrayList<>();
-        BufferedReader possible_words_in = new BufferedReader(new FileReader(new File(args[0])));
+        BufferedReader possible_words_in = new BufferedReader(new FileReader(new File("en_US.dic")));
         ArrayList<String> possible_words = new ArrayList<>();
         
         typos_in.lines().forEach(typo_words::add);
@@ -31,7 +31,25 @@ public class Autocorrector {
         
         ArrayList<String> corrected_words = new ArrayList<>();
         
-        // TODO: populate array list with corrections based on Damerau-Levenshtein distance
+        // TODO: populate array list with suggestions based on closest
+        // Damerau-Levenshtein distance, ignoring case, but preserving case of
+        // suggestions        
+        for(String typo_word: typo_words){
+            int closest_dl_distance = Integer.MAX_VALUE;
+            String closest_word = typo_word;
+            for(String possible_word: possible_words) {
+                int cur_distance = DamerauLevenshteinComparer.getDistance(typo_word, possible_word.toLowerCase());
+                if(cur_distance < closest_dl_distance) {
+                    closest_dl_distance = cur_distance;
+                    closest_word = possible_word;
+                }
+            }
+            if(closest_dl_distance < 2) {
+                corrected_words.add(closest_word);
+            } else {
+                corrected_words.add("UNKNOWN");
+            }
+        }
         
         corrected_words.forEach(System.out::println);
     }
